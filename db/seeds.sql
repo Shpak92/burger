74a900f150832c6528f8a7f646bd1c20e99f707d
George Shpak <georgeshpak@Georges-MacBook-Air.local>
2019-05-21 22:47:43 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burger_db
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger between 2 Grilled Cheesed Sandwiches');
+
